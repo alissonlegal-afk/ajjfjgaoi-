e27268108323bc5125fe8c8805f38b1dcb01c5cc
alissonlegal-afk <alisson.fagundes.andrade@escola.pr.gov.br>
2025-09-01 08:01:53 -0300

    dasd

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const calcular = document.getElementById('calcular')
+const calcular = document.getElementById('calcular');
 
 function imc(){
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('reultado');
+    const resultado = document.getElementById
+    ('resultado');
 
-    const valorIMC = (peso/(altura*altura)).toFixed
+    const valorIMC = (peso/(altura*altura)).toFixed(1);
 
-    resultado.innerHTML = `${nome} seu IMC é ${valorIMC}!`
+    if(nome != "" && altura != "" && peso != ""){
+       
+        const valorIMC = (peso/(altura*altura)).toFixed(1);
+        let = classificacao = "";
+
+        if(valorIMC < 18.5){
+            classificacao = "abaixo do peso";
+        }else
+    }
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
 }
 
 calcular.addEventListener('click',imc);
